d2b3ed7daabf1a6e652bd3362c5541732e21839d
mdjassia <mdjassia@gmail.com>
2024-06-06 11:01:38 +0100

    k

--- assia-project-v0.2/frontend/src/pages/PeripheralsPage.tsx
+++ assia-project-v0.2/frontend/src/pages/PeripheralsPage.tsx
@@ -3,7 +3,7 @@ import Card from "../components/UI/Card";
 import Table from "../components/UI/Tables/Table";
 import TableCell from "../components/UI/Tables/TableCell";
 import TableRow from "../components/UI/Tables/TableRow";
-import Badge from "../components/UI/Badge";
+
 import DeleteButton from "../components/UI/Buttons/DeleteButton";
 import Button from "../components/UI/Buttons/Button";
 import { useState, useEffect } from "react";
@@ -22,9 +22,6 @@ function PeripheralsPage() {
     });
   }, []);
 
-
-  
-
   const [deviceUsers, setDeviceUsers] = useState<any[]>([]);
   useEffect(() => {
     axios.get(`${baseURL}/DeviceUsers`).then((response) => {
@@ -32,7 +29,7 @@ function PeripheralsPage() {
     });
   }, []);
 
-   const [users, setUsers] = useState<any[]>([]);
+  const [users, setUsers] = useState<any[]>([]);
   useEffect(() => {
     axios.get(`${baseURL}/users`).then((response) => {
       setUsers(response.data);
@@ -42,31 +39,44 @@ function PeripheralsPage() {
   const getUsersForDevice = (deviceId: string) => {
     return deviceUsers
       .filter(du => du.device_id === deviceId)
-      .map(du => users.find(user => user.email === du.user_id)?.username || "Unknown User")
-      .join(", ");
+      .map(du => users.find(user => user.email === du.user_id)?.username || "Unknown User");
   };
-  
-  
+
+  const [classes, setClasses] = useState<any[]>([]);
+  useEffect(() => {
+    axios.get(`${baseURL}/class`).then((response) => {
+      setClasses(response.data);
+    });
+  }, []);
+
+  const getClassNameByGuid = (guid: string) => {
+    const classe = classes.find(c => c.guid === guid);
+    return classe ? classe.type : "Unknown Class";
+  };
 
   return (
     <>
       <h3 className="mb-4"> Périphériques </h3>
       <Card title="Périphériques" subtitle="Liste des périphériques" action={addButton}>
         <Table fields={["#", "id", "nom peripherique", "classe peripherique", "utilisateurs"]}>
-          {
-            peripherique.map((x, i) => (
-              <TableRow >
-                <TableCell>{i + 1}</TableCell>
-                <TableCell>{x.iddevice}</TableCell>
-                <TableCell>{x.named}</TableCell>
-                <TableCell>{x.classi_id}</TableCell>
-                <TableCell>{getUsersForDevice(x.iddevice)}</TableCell>
-                <TableCell>
-                  <DeleteButton onClick={() => null} />
-                </TableCell>
-              </TableRow>
-            ))
-          }
+          {peripherique.map((x, i) => (
+            <TableRow key={x.iddevice}>
+              <TableCell>{i + 1}</TableCell>
+              <TableCell>{x.iddevice}</TableCell>
+              <TableCell>{x.named}</TableCell>
+              <TableCell>{getClassNameByGuid(x.classi_id)}</TableCell>
+              <TableCell className="list">
+                <ul className="list">
+                  {getUsersForDevice(x.iddevice).map((username, index) => (
+                    <li key={index}>{username}</li>
+                  ))}
+                </ul>
+              </TableCell>
+              <TableCell>
+                <DeleteButton onClick={() => null} />
+              </TableCell>
+            </TableRow>
+          ))}
         </Table>
       </Card>
       <ModalExample isOpen={openModal === "add"} close={() => setOpenModal("")} />
