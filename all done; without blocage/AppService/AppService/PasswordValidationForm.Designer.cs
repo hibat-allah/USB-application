b0b8d41999112cd5bf3b6884490a38d172425e38
hibat-allah <hibatallahnibou@gmail.com>
2024-06-11 17:58:39 +0100

    before usb blocking feature

---
+++ all done; without blocage/AppService/AppService/PasswordValidationForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AppService
+{
+    partial class PasswordValidationForm
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordValidationForm));
+
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox confirmPasswordTextBox;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label confirmPasswordLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.confirmPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.confirmPasswordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(16, 49);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(345, 22);
+            this.passwordTextBox.TabIndex = 0;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // confirmPasswordTextBox
+            // 
+            this.confirmPasswordTextBox.Location = new System.Drawing.Point(16, 98);
+            this.confirmPasswordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.confirmPasswordTextBox.Name = "confirmPasswordTextBox";
+            this.confirmPasswordTextBox.Size = new System.Drawing.Size(345, 22);
+            this.confirmPasswordTextBox.TabIndex = 1;
+            this.confirmPasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(16, 135);
+            this.messageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(0, 16);
+            this.messageLabel.TabIndex = 2;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(263, 172);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(100, 28);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(16, 18);
+            this.instructionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(191, 16);
+            this.instructionLabel.TabIndex = 4;
+            this.instructionLabel.Text = "Please enter a valid password:";
+            // 
+            // confirmPasswordLabel
+            // 
+            this.confirmPasswordLabel.AutoSize = true;
+            this.confirmPasswordLabel.Location = new System.Drawing.Point(16, 74);
+            this.confirmPasswordLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.confirmPasswordLabel.Name = "confirmPasswordLabel";
+            this.confirmPasswordLabel.Size = new System.Drawing.Size(118, 16);
+            this.confirmPasswordLabel.TabIndex = 5;
+            this.confirmPasswordLabel.Text = "Confirm Password:";
+            // 
+            // PasswordValidationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 215);
+            this.Controls.Add(this.confirmPasswordLabel);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.confirmPasswordTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Icon = global::AppService.Properties.Resources.icon;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "PasswordValidationForm";
+            this.Text = "Password Entry";
+            this.Load += new System.EventHandler(this.PasswordValidationForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+
+}
