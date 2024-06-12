a721ad40f0272b667c822aba3aad3b63ed9d68b4
hibat-allah <hibatallahnibou@gmail.com>
2024-06-12 01:44:31 +0100

    blocking draft

---
+++ blocking code/blocage/blocage/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blocage
+{
+
+        public class Users
+        {
+            // Attributs
+            public string Name { get; set; }
+            public string Password { get; set; }
+
+            // Constructeur
+            public Users(string name, string password)
+            {
+                Name = name;
+                Password = password;
+            }
+        }
+    
+}
